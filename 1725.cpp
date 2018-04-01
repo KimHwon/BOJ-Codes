94786b3ded2933e60dae93fd1abb6c2baaf7cdf4
KimHwon <233hwon@naver.com>
2018-04-02 02:11:43 +0900

    Update 1725.cpp
    Change spaces to tabs

--- 1725.cpp
+++ 1725.cpp
@@ -21,12 +21,12 @@ int area(int left, int right)
 	int r = MAX(area(left, center), area(center + 1, right));	// 양쪽으로 분할하여 재귀 호출
 
 	int low = center, high = center + 1;
-	int height = MIN(hist[low], hist[high]);	// 가운데 2칸을 포함하는 경우
-												// 여기서부터 확장해 나가면서 계산해본다
+	int height = MIN(hist[low], hist[high]);		// 가운데 2칸을 포함하는 경우
+								// 여기서부터 확장해 나가면서 계산해본다
 	r = MAX(r, height * 2);
 	while (left < low || high < right)
 	{
-		if (high < right && (low == left || hist[low - 1] < hist[high + 1]))	// 높이가 높은쪽 우선 확장
+		if (high < right && (low == left || hist[low - 1] < hist[high + 1])) // 높이가 높은쪽 우선 확장
 		{
 			++high;
 			height = MIN(height, hist[high]);
@@ -37,7 +37,7 @@ int area(int left, int right)
 			height = MIN(height, hist[low]);
 		}
 
-		r = MAX(r, height * (high - low + 1));	// 확장한 후의 넓이
+		r = MAX(r, height * (high - low + 1));		// 확장한 후의 넓이
 	}
 
 	return r;
