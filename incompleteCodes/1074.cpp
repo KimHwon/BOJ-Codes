0ca9f7535a3694af971c7680bef2722ac4be4f3e
KimHwon <233hwon@naver.com>
2018-04-02 01:50:22 +0900

    Update incompleteCodes/1074.cpp
    Add comment

--- incompleteCodes/1074.cpp
+++ incompleteCodes/1074.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int format[4][4] = 
+int format[4][4] = 		// 4x4칸 이하일 경우, 더 이상 재귀호출하는 대신 참조로 풀이
 {
 	{ 0, 1, 4, 5 },
 	{ 2, 3, 6, 7 },
@@ -27,21 +27,21 @@ void count(int x, int y, int sz)
 		return;
 	}
 
-	if ((x <= _x && _x < x + (sz / 2)) && (y <= _y && _y < y + (sz / 2)))
+	if ((x <= _x && _x < x + (sz / 2)) && (y <= _y && _y < y + (sz / 2)))			// 목표값이 왼쪽위 영역에 있을 경우
 	{
 		count(x, y, sz / 2);
 	}
-	else if ((x + (sz / 2) <= _x && _x < x + sz) && (y <= _y && _y < y + (sz / 2)))
+	else if ((x + (sz / 2) <= _x && _x < x + sz) && (y <= _y && _y < y + (sz / 2)))		// 목표값이 오른쪽위 영역에 있을 경우
 	{
 		sum += (sz / 2) * (sz / 2);
 		count(x + (sz / 2), y, sz / 2);
 	}
-	else if ((x <= _x && _x < x + (sz / 2)) && (y + (sz / 2) <= _y && _y < y + sz))
+	else if ((x <= _x && _x < x + (sz / 2)) && (y + (sz / 2) <= _y && _y < y + sz))		// 목표값이 왼쪽아래 영역에 있을 경우
 	{
 		sum += (sz / 2) * (sz / 2) * 2;
 		count(x, y + (sz / 2), sz / 2);
 	}
-	else
+	else											// 목표값이 오른쪽아래 영역에 있을 경우
 	{
 		sum += (sz / 2) * (sz / 2) * 3;
 		count(x + (sz / 2), y + (sz / 2), sz / 2);
