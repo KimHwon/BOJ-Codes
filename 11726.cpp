85d279e5861adb372524f778d266fccdcccf64ce
KimHwon <233hwon@naver.com>
2018-04-12 02:04:28 +0900

    Update 11726.cpp
    Add comment

--- 11726.cpp
+++ 11726.cpp
@@ -1,3 +1,4 @@
+// C++11
 #include <iostream>
 
 using namespace std;
@@ -15,10 +16,10 @@ int main()
 	block[0] = block[1] = 1;
 	for (int i = 2; i <= n; ++i)
 	{
-		block[i] = (block[i - 1] + block[i - 2]) % 10007;
+		block[i] = (block[i - 1] + block[i - 2]) % 10007; // overflow가 일어나므로 각 계산마다 10007을 나눠 int영역 안으로 계산
 	}
 
-	cout << (block[n] % 10007);
+	cout << (block[n] % 10007); // overflow가 일어나므로 각 계산마다 10007을 나눠 int영역 안으로 계산
 
 	return 0;
 }
