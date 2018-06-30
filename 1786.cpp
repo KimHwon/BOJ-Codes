d63bdd00789e0fbfbc6fa0104eab3189ee7fff5a
KimHwon <233hwon@naver.com>
2018-06-30 20:48:10 +0900

    Fix typo

--- 1786.cpp
+++ 1786.cpp
@@ -14,7 +14,7 @@ int main()
 	getline(cin, str1);
 	getline(cin, str2);
 
-	vector<int> jmp(str2.size(), 0);
+	vector<int> pi(str2.size(), 0);
 	vector<int> rslt;
 
 	int eq = 0;
@@ -22,12 +22,12 @@ int main()
 	{
 		while (eq && str2[i] != str2[eq])
 		{
-			eq = jmp[eq - 1];
+			eq = pi[eq - 1];
 		}
 		if (str2[i] == str2[eq])
 		{
 			++eq;
-			jmp[i] = eq;
+			pi[i] = eq;
 		}
 	}
 
@@ -36,7 +36,7 @@ int main()
 	{
 		while (eq && str1[i] != str2[eq])
 		{
-			eq = jmp[eq - 1];
+			eq = pi[eq - 1];
 		}
 		if (str1[i] == str2[eq])
 		{
@@ -47,7 +47,7 @@ int main()
 				rslt.push_back(i - eq + 1);
 				++cnt;
 
-				eq = jmp[eq - 1];
+				eq = pi[eq - 1];
 			}
 		}
 	}
